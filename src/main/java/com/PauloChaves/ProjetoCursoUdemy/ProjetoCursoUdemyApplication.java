00ef9923906583fc491eac7249118c9f1b502a0d
Paulo Miron <paulomiron16@gmail.com>
2021-04-05 13:49:53 -0300

    Primeiro Commit, Criando projeto

---
+++ src/main/java/com/PauloChaves/ProjetoCursoUdemy/ProjetoCursoUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.PauloChaves.ProjetoCursoUdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCursoUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCursoUdemyApplication.class, args);
+	}
+
+}
