1fcf26d81ca839184a7a51c317618e97d8a62b2d
Paulo Miron <paulomiron16@gmail.com>
2021-04-06 11:14:47 -0300

    Criando classe categoria

---
+++ src/main/java/com/PauloChaves/ProjetoCursoUdemy/entities/Categoria.java
@@ -0,0 +1,53 @@
+package com.PauloChaves.ProjetoCursoUdemy.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+
+    public Categoria(){}
+
+    public Categoria(Long id,String nome) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/com/PauloChaves/ProjetoCursoUdemy/resources/CategoriaResource.java
+++ src/main/java/com/PauloChaves/ProjetoCursoUdemy/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.PauloChaves.ProjetoCursoUdemy.resources;
 
+import com.PauloChaves.ProjetoCursoUdemy.entities.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar(){
-        return "Testando GET";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1L,"Informatica");
+        Categoria cat2 = new Categoria(2L,"Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
