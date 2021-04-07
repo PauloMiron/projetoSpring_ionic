f1e72778fa4f0564daacdc0fd52e4820c73a14eb
Paulo Miron <paulomiron16@gmail.com>
2021-04-07 20:18:16 -0300

    Cliente, Tipo cliente,Telefones e enderecos

---
+++ src/main/java/com/PauloChaves/ProjetoCursoUdemy/entities/Cliente.java
@@ -0,0 +1,97 @@
+package com.PauloChaves.ProjetoCursoUdemy.entities;
+
+import com.PauloChaves.ProjetoCursoUdemy.entities.enums.TipoCliente;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpfOuCnpj;
+    private Integer tipo;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Endereco> enderecos = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(name = "TELEFONE")
+    private Set<String> telefones = new HashSet<>();
+
+    public Cliente(){}
+
+    public Cliente(Long id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpfOuCnpj = cpfOuCnpj;
+        this.tipo = tipo.getCod();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
+
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        this.cpfOuCnpj = cpfOuCnpj;
+    }
+
+    public TipoCliente getTipo() {
+        return TipoCliente.toEnum(tipo);
+    }
+
+    public void setTipo(TipoCliente tipo) {
+        this.tipo = tipo.getCod();
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public Set<String> getTelefones() {
+        return telefones;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/com/PauloChaves/ProjetoCursoUdemy/teste/Teste.java
+++ src/main/java/com/PauloChaves/ProjetoCursoUdemy/teste/Teste.java
@@ -1,13 +1,9 @@
 package com.PauloChaves.ProjetoCursoUdemy.teste;
 
-import com.PauloChaves.ProjetoCursoUdemy.entities.Categoria;
-import com.PauloChaves.ProjetoCursoUdemy.entities.Cidade;
-import com.PauloChaves.ProjetoCursoUdemy.entities.Estado;
-import com.PauloChaves.ProjetoCursoUdemy.entities.Produto;
-import com.PauloChaves.ProjetoCursoUdemy.repository.CategoriaRepository;
-import com.PauloChaves.ProjetoCursoUdemy.repository.CidadeRepository;
-import com.PauloChaves.ProjetoCursoUdemy.repository.EstadoRepository;
-import com.PauloChaves.ProjetoCursoUdemy.repository.ProdutoRepository;
+import ch.qos.logback.core.net.server.Client;
+import com.PauloChaves.ProjetoCursoUdemy.entities.*;
+import com.PauloChaves.ProjetoCursoUdemy.entities.enums.TipoCliente;
+import com.PauloChaves.ProjetoCursoUdemy.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -28,6 +24,10 @@ public class Teste implements CommandLineRunner{
         private EstadoRepository estadoRepository;
         @Autowired
         private CidadeRepository cidadeRepository;
+        @Autowired
+        private ClienteRepository clienteRepository;
+        @Autowired
+        private EnderecoRepository enderecoRepository;
 
         @Override
         public void run(String... args) throws Exception {
@@ -58,11 +58,22 @@ public class Teste implements CommandLineRunner{
 
             est1.getCidades().addAll(Arrays.asList(c1));
             est2.getCidades().addAll(Arrays.asList(c2,c3));
-            
+
             estadoRepository.saveAll(Arrays.asList(est1,est2));
             cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 
+            Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","36378912377", TipoCliente.PESSOAFISICA);
 
+            cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
+
+            Endereco e1 = new Endereco(null,"Rua flores","300","Apto 303","Jardim","38220834",cli1,c1);
+            Endereco e2 = new Endereco(null,"Avenida Matos","105","Sala 800","Centro","3877012",cli1,c2);
+
+
+            cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+
+            clienteRepository.saveAll(Arrays.asList(cli1));
+            enderecoRepository.saveAll(Arrays.asList(e1,e2));
 
         }
     }
