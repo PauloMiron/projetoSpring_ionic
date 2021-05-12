865de7124944c4bfb761096f6f17121b6d7a35b4
Paulo Miron <paulomiron16@gmail.com>
2021-05-12 00:02:35 -0300

    Salvando primeiro no S3

--- src/main/java/com/PauloChaves/ProjetoCursoUdemy/ProjetoCursoUdemyApplication.java
+++ src/main/java/com/PauloChaves/ProjetoCursoUdemy/ProjetoCursoUdemyApplication.java
@@ -1,5 +1,7 @@
 package com.PauloChaves.ProjetoCursoUdemy;
 
+import com.PauloChaves.ProjetoCursoUdemy.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjetoCursoUdemyApplication  implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoCursoUdemyApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("D:\\Imagens\\carro.jpg");
 	}
 }
