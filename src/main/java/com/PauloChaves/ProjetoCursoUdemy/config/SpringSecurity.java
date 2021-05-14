a85bb06920a93193ad9239a09c6f79b57bf9eadf
Paulo Miron <paulomiron16@gmail.com>
2021-05-14 18:39:18 -0300

    Salvando URL imagem em cliente

--- src/main/java/com/PauloChaves/ProjetoCursoUdemy/config/SpringSecurity.java
+++ src/main/java/com/PauloChaves/ProjetoCursoUdemy/config/SpringSecurity.java
@@ -47,7 +47,6 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
     private static final String[] PUBLIC_MATCHES_POST = {
             "/clientes",
-            "/clientes/picture",
             "/auth/forgot/**"
     };
 
--- src/main/java/com/PauloChaves/ProjetoCursoUdemy/services/ClienteService.java
+++ src/main/java/com/PauloChaves/ProjetoCursoUdemy/services/ClienteService.java
@@ -118,7 +118,19 @@ public class ClienteService {
     }
 
     public URI uploadProfilePicture(MultipartFile multipartFile){
-        return s3Service.uploadFile(multipartFile);
+        UserSS user = UserService.authenticated();
+        if (user == null){
+            throw new AuthorizationExceptions("Acesso negado");
+        }
+
+        URI uri = s3Service.uploadFile(multipartFile);
+
+        Cliente cli = find(user.getId());
+        cli.setImageUrl(uri.toString());
+        repo.save(cli);
+
+
+        return uri;
     }
 }
 
